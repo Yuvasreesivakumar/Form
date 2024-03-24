f3785441ad16f894ff2fcd3a4b48c952c7e6a6d3
Yuvasreesivakumar <yuvasreesivakumar04@gmail.com>
2024-03-24 09:54:59 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,57 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   title = 'form';
+  userArray: Array<any> = [];
+  user: any = {};
+  age: number | null = null;
+  
+
+  Dept=['FullStack','Cybersecurity','Data Analyst','Software Development','Cloud Computing']
+
+
+  onClick() {
+    this.calculateAge();
+    this.userArray.push({
+      name: this.user.name,
+      id: this.user.id,
+      dept: this.user.dept,
+      salary: this.user.salary,
+      experience: this.user.experience,
+      dob:this.user.dob,
+      age:this.age,
+
+
+    });
+
+  }
+
+  calculateAge() {
+    if (this.user.dob) {
+      const today = new Date();
+      const birthDate = new Date(this.user.dob);
+      let age = today.getFullYear() - birthDate.getFullYear();
+      const monthDiff = today.getMonth() - birthDate.getMonth();
+
+      if (monthDiff < 0 || (monthDiff === 0 && today.getDate() < birthDate.getDate())) {
+        age--;
+      }
+
+      this.age = age;
+    } else {
+      this.age = null;
+    }
+  }
+  onDelete(index: number) {
+    if (index >= 0 && index < this.userArray.length) {
+      this.userArray.splice(index, 1);
+    }
+  }
+
+  onSubmit(eform: any) {
+    console.log(eform.value);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,17 +4,24 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { FormsModule } from '@angular/forms';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
+ declarations: [
+    AppComponent,
+    HeaderComponent,
+    FooterComponent
+ ],
+ imports: [
     BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [
+    AppRoutingModule,
+    FormsModule
+ ],
+ providers: [
     provideClientHydration()
-  ],
-  bootstrap: [AppComponent]
+ ],
+ bootstrap: [AppComponent]
 })
 export class AppModule { }
